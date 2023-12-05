f13e31361fa00113c64f89a2aeae93892f17cca4
hafinamei <hafina.meilawati@gmail.com>
2023-12-06 04:57:37 +0700

    menambahkan desain delivery status

---
+++ app/src/main/java/com/example/kel_10_adminfeed/AddItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kel_10_adminfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item)
+    }
+
+}
