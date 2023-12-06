f58045bce0567b8387abad7bc4a6e50a6759b301
hafinamei <hafina.meilawati@gmail.com>
2023-12-06 10:26:29 +0700

    menambahkan delivery item dan adapter

---
+++ app/src/main/java/com/example/kel_10_adminfeed/OutForDeliveryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kel_10_adminfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OutForDeliveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_out_for_delivery)
+    }
+}
---
+++ app/src/main/java/com/example/kel_10_adminfeed/adapter/DeliveryAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.kel_10_adminfeed.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class DeliveryAdapter(): RecyclerView.Adapter<DeliveryAdapter.DeliveryHolder>() {
+    inner class DeliveryHolder(private val binding: DeliveryItemBinding) :RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeliveryHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DeliveryHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
