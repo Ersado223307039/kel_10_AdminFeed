7725820a94925deb98221a4a8c9185d20698103c
hafinamei <hafina.meilawati@gmail.com>
2023-12-06 09:30:45 +0700

    menambahkan all item adapter, dan add item

--- app/src/main/java/com/example/kel_10_adminfeed/AddItemActivity.kt
+++ app/src/main/java/com/example/kel_10_adminfeed/AddItemActivity.kt
@@ -2,11 +2,33 @@ package com.example.kel_10_adminfeed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.ActivityResultLauncher
+import com.example.kel_10_adminfeed.databinding.ActivityAddItemBinding
 
 class AddItemActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_item)
+    private val binding: ActivityAddItemBinding by lazy {
+        ActivityAddItemBinding.inflate(layoutInflater)
     }
 
+    private lateinit var pickImage: ActivityResultLauncher<String?>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        pickImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            if (uri != null) {
+                binding.selectedImage.setImageURI(uri)
+            }
+        }
+
+        binding.selectImage.setOnClickListener {
+            pickImage.launch("image/*")
+        }
+
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+    }
 }
---
+++ app/src/main/java/com/example/kel_10_adminfeed/AllItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kel_10_adminfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_item)
+    }
+}
