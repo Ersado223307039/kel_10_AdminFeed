e4e43e2cddfc0b5835c2bad69b0a1101e01474d2
Ersado223307039 <ersadocb2505@gmail.com>
2023-12-07 21:49:23 +0700

    perbaikan pada add Item

--- app/src/main/java/com/example/kel_10_adminfeed/AddItemActivity.kt
+++ app/src/main/java/com/example/kel_10_adminfeed/AddItemActivity.kt
@@ -4,31 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import com.bumptech.glide.Glide
+import com.example.kel_10_adminfeed.databinding.ActivityAddItemBinding
+import com.example.kel_10_adminfeed.databinding.ActivityAllItemBinding
 
 
 class AddItemActivity : AppCompatActivity() {
-//    private val binding: ActivityAddItemBinding by lazy {
-//        ActivityAddItemBinding.inflate(layoutInflater)
-//    }
 
-//    private lateinit var pickImage: ActivityResultLauncher<String?>
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(binding.root)
-//
-//        pickImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-//            if (uri != null) {
-//                binding.selectedImage.setImageURI(uri)
-//            }
-//        }
-//
-//        binding.selectImage.setOnClickListener {
-//            pickImage.launch("image/*")
-//        }
-//
-//        binding.backButton.setOnClickListener {
-//            finish()
-//        }
-//    }
+    private val binding: ActivityAddItemBinding by lazy {
+        ActivityAddItemBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.selectImage.setOnClickListener{
+            pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+
+        }
+
+     }
+    val pickImage = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){uri ->
+        if (uri !=null){
+            binding.selectImage.setImageURI(uri)
+        }
+
+
+
+    }
+
 }
--- app/src/main/java/com/example/kel_10_adminfeed/MainActivity.kt
+++ app/src/main/java/com/example/kel_10_adminfeed/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.kel_10_adminfeed
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kel_10_adminfeed.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  val binding:ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.addMenu.setOnClickListener {
+            val intent = Intent(this,AddItemActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
