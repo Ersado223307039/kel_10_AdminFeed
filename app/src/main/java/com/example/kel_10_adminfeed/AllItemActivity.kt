cdac745aa9ab79975140149b47d8a396d0ccaa34
Ersado223307039 <ersadocb2505@gmail.com>
2023-12-12 20:57:15 +0700

    menyambungkan item view ke firebase

--- app/src/main/java/com/example/kel_10_adminfeed/AllItemActivity.kt
+++ app/src/main/java/com/example/kel_10_adminfeed/AllItemActivity.kt
@@ -2,31 +2,67 @@ package com.example.kel_10_adminfeed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.kel_10_adminfeed.adapter.AddItemAdapter
+import com.example.kel_10_adminfeed.adapter.MenuItemAdapter
 import com.example.kel_10_adminfeed.databinding.ActivityAllItemBinding
+import com.example.kel_10_adminfeed.model.AllMenu
+import com.google.firebase.database.*
 
 class AllItemActivity : AppCompatActivity() {
+    private lateinit var databaseReference:DatabaseReference
+    private lateinit var database:FirebaseDatabase
+    private var menuItems:ArrayList<AllMenu> = ArrayList()
     private val binding: ActivityAllItemBinding by lazy {
         ActivityAllItemBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val menuFoodName = listOf("burger","sesns","chickn","item","chickn","item")
-        val menuItemPrice = listOf("$4","$3","8","$4","$3","8")
-        val menuIMage= listOf(
-            R.drawable.menu1,
-            R.drawable.menu2,
-            R.drawable.menu3,
-            R.drawable.menu4,
-            R.drawable.menu5,
-            R.drawable.menu5
+        databaseReference =FirebaseDatabase.getInstance().reference
+        retriveMenuItem()
 
-        )
-        val adapter= AddItemAdapter(ArrayList(menuFoodName), ArrayList(menuItemPrice),
-            ArrayList(menuIMage)
-        )
+
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+
+
+    }
+
+    private fun retriveMenuItem() {
+        database= FirebaseDatabase.getInstance()
+        val foodRef:DatabaseReference=database.reference.child("menu")
+
+        //fetch database
+
+        foodRef.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //clear existing data
+               menuItems.clear()
+                //loop each food item
+               for (foodSnapShot in snapshot.children){
+                   val menuItem = foodSnapShot.getValue(AllMenu::class.java)
+                   menuItem?.let {
+                       menuItems.add(it)
+                   }
+               }
+                setAdapter()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("DatabaseError","Error: ${error.message}")
+
+            }
+
+
+        })
+
+    }
+    private fun setAdapter() {
+        val adapter= MenuItemAdapter(this@AllItemActivity,menuItems,databaseReference)
+
         binding.MenuRecyclerview.layoutManager=LinearLayoutManager(this)
         binding.MenuRecyclerview.adapter=adapter
 
--- app/src/main/java/com/example/kel_10_adminfeed/adapter/MenuItemAdapter.kt
+++ app/src/main/java/com/example/kel_10_adminfeed/adapter/MenuItemAdapter.kt
@@ -1,12 +1,23 @@
 package com.example.kel_10_adminfeed.adapter
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.kel_10_adminfeed.databinding.ItemItemBinding
+import com.example.kel_10_adminfeed.model.AllMenu
+import com.google.firebase.database.DatabaseReference
 
 
-class AddItemAdapter (private val MenuItemName:ArrayList<String>, private val MenuItemPrice:ArrayList<String>, private val MenuItemImage:ArrayList<Int>):RecyclerView.Adapter<AddItemAdapter.AddItemViewHolder>(){
-    private val itemQuantities = IntArray(MenuItemName.size){1}
+class MenuItemAdapter(
+    private val context: Context,
+    private val menuList: ArrayList<AllMenu>,
+    databaseReference: DatabaseReference,
+
+
+    ):RecyclerView.Adapter<MenuItemAdapter.AddItemViewHolder>(){
+    private val itemQuantities = IntArray(menuList.size){1}
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddItemViewHolder {
@@ -19,16 +30,20 @@ class AddItemAdapter (private val MenuItemName:ArrayList<String>, private val Me
     override fun onBindViewHolder(holder: AddItemViewHolder, position: Int) {
         holder.bind(position)
     }
-    override fun getItemCount(): Int = MenuItemName.size
+    override fun getItemCount(): Int = menuList.size
 
 
     inner class AddItemViewHolder(private val binding: ItemItemBinding):RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
             binding.apply {
                 val quantity = itemQuantities[position]
-                foodNameTextView.text=MenuItemName[position]
-                priceTextView.text= MenuItemPrice[position]
-                foodImageView.setImageResource(MenuItemImage[position])
+                val menuItem= menuList[position]
+                val uriString =menuItem.foodImage
+                val uri = Uri.parse(uriString)
+                foodNameTextView.text=menuItem.foodName
+                priceTextView.text=menuItem.foodPrice
+                Glide.with(context).load(uri).into(foodImageView)
+
                 quantityTextView.text=quantity.toString()
 
                 minusButton.setOnClickListener {
@@ -58,11 +73,11 @@ class AddItemAdapter (private val MenuItemName:ArrayList<String>, private val Me
             }
         }
         private fun deleteQuantity(position: Int) {
-            MenuItemName.removeAt(position)
-            MenuItemPrice.removeAt(position)
-            MenuItemImage.removeAt(position)
+            menuList.removeAt(position)
+            menuList.removeAt(position)
+            menuList.removeAt(position)
             notifyItemRemoved(position)
-            notifyItemRangeChanged(position,MenuItemName.size)
+            notifyItemRangeChanged(position,menuList.size)
         }
 
     }
